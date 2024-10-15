16614e7c46a7ef5e37d4a7e426b696a051e0747b
Wilkolak3 <patrykjanusz@onet.pl>
2024-10-15 23:19:48 +0200

    całość

---
+++ Tablica.py
@@ -0,0 +1,50 @@
+
+import random
+
+
+class Tablica:
+    def __init__(self, n):
+        self.n = n
+        self.tab = [0] * n
+
+    def wypelnij(self, a, b):
+        for i in range(self.n):
+            self.tab[i] = random.randint(a, b)
+
+    def minimum(self):
+        mini = self.tab[0]
+        for i in self.tab:
+            if i < mini:
+                min = i
+        return min
+
+    def maksimum(self):
+        maks = self.tab[0]
+        for i in self.tab:
+            if i > maks:
+                maks = i
+        return maks
+
+    def maksimum2(self):
+        if self.tab[0] > self.tab[1]:
+            maks = self.tab[0]
+            maks2 = self.tab[1]
+        else:
+            maks = self.tab[1]
+            maks2 = self.tab[0]
+        for i in self.tab[2:]:
+            if i > maks:
+                maks2 = maks
+                maks = i
+            elif i > maks2 and i != maks:
+                maks2 = i
+        return maks2
+
+    def znajdz(self, a):
+        for i in range(len(self.tab)):
+            if self.tab[i] == a:
+                return i
+        return -1
+
+    def __str__(self):
+        return str(self.tab)
---
+++ main.py
@@ -0,0 +1,21 @@
+from Tablica import Tablica
+
+tab = Tablica(10)
+tab.wypelnij(1, 10)
+print("Tablica: ", tab)
+
+mini = tab.minimum()
+print("Minimum: ", min)
+
+maks = tab.maksimum()
+print("Maksimum: ", maks)
+
+
+maks2 = tab.maksimum2()
+print("Maksimum2: ", maks2)
+
+szukana = 5
+index = tab.znajdz(szukana)
+print("Wartość szukana: ", szukana, "\n Index: ", index)
+
+
